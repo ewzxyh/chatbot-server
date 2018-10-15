273283679390ad677605326dab7139c70f699223
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-10-15 18:51:44 +0200

    added 're-send verify email'

--- models/emailService.js
+++ models/emailService.js
@@ -944,7 +944,7 @@ class EmailService {
                             <!-- <br> welcome on TileDesk.com. -->
                             <br><br> Thank you for signin up with TileDesk.
                             <br><br> To complete the setup, <span><a href="https://support.tiledesk.com/dashboard/#/verify/email/${savedUser._id}"> click here to verify your email address. </a> </span>
-                            <br><br>Give us your feedback! We need your advice. Send an email to redacted@example.invalid
+                            <br><br>Give us your feedback! We need your advice. Send an email to <a href="mailto:redacted@example.invalid">redacted@example.invalid</a>
                             <br><br> Team TileDesk
                           </td>
                         </tr>
