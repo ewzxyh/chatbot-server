2b80b92a1146f48783afc54a3298054ace89cbbe
andrea leo <andrea.leo83@gmail.com>
2020-08-03 11:07:39 +0200

    disable node_modules cache

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   'secret':'nodeauthsecret',
-  'database': 'mongodb://localhost:27017/tiledesk',
+  'database': '[REDACTED_CREDENTIAL_URL]',
   'databaselogs': 'mongodb://localhost:27017/tiledesk-logs',
   'databasetest': 'mongodb://localhost:27017/tiledesk-test'
 };
