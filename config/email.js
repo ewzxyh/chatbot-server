3ad5ff44ed7249daa66c6e7677fa80ced3b5a507
andrea leo <andrea.leo83@gmail.com>
2021-05-27 17:13:56 +0200

    emailService added EMAIL_REPLY_TO and EMAIL_REPLY_TO_DOMAIN parameters;

--- config/email.js
+++ config/email.js
@@ -3,5 +3,6 @@ module.exports = {
   'username': 'redacted@example.invalid',
   'from': 'Tiledesk Notification <redacted@example.invalid>',
   'bcc': 'redacted@example.invalid',
+  'replyToDomain': 'test.smtp.frontiere21.it',
   'baseUrl':'https://console.tiledesk.com/v2/dashboard',
 };
