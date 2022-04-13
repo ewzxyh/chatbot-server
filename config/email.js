a305499fa191b4e386accca712c4c05d38b80027
Andrea Leo <andrea.leo83@gmail.com>
2022-04-13 13:06:59 +0200

    - Added tag field to the project_user - Removed default BCC from email

--- config/email.js
+++ config/email.js
@@ -2,7 +2,7 @@ module.exports = {
   'host':'smtp.mailgun.org',
   'username': 'redacted@example.invalid',
   'from': 'Tiledesk Notification <redacted@example.invalid>',
-  'bcc': 'redacted@example.invalid',
+  'bcc': '',
   'baseUrl':'https://console.tiledesk.com/v2/dashboard',
   'replyEnabled' : false,
   'inboundDomain': 'tickets.tiledesk.com'
